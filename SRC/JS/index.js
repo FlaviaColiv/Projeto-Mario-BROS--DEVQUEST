040e1d4bfabedcbd585fd301b0f040fd569e404a
Flavia Oliveira <flaviacoliv@hotmail.com>
2023-02-21 21:40:02 -0300

    Atualização 1.0.0

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -1,1 +1,15 @@
-alert('Oi');
+/*
+Objetivos:
+
+1-Quando o usuário clicar no botão de ver trailer, devemos abrir a modal com o vídeo do Trailer.
+    1.1 - dar um jeito de pegar o elemento que representa o botão na tela usando JS.
+    1.2 - dar um jeito de identificar quando o usuário clicar no botão.
+    1.3 - dar um jeito de pegar o elemento da modal no JS.
+    1.4 - abrir a modal na tela.
+
+2- quando o usuário clicar no x, devemos fechar a modal.
+    2.1 - dar um jeito de pegar o elemento de fechar modal usando o js.
+    2.2 - dar um jeito de identificar quando o usuario clicar no x.
+    2.3 - fechar a modal.
+    
+*/
